611d9f97406f9c7a36567cb4f87e11c0d0c0ea0c
TetzkatLipHoka <TLH@freenet.de>
2024-10-17 00:42:38 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Frame4Plugin")]
+[assembly: AssemblyTitle("Frame4")]
 [assembly: AssemblyDescription("Plugin that implements frame4 for ReClass.NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TLH")]
